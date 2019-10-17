3334eecebe6550bdf6e04cf03dd0da32ff6edc97
Rolf Johansson <rolf.johansson@chalmers.se>
2019-10-17 15:03:44 +0200

    Add API calls for categories and category groups.

--- canvas/index.js
+++ canvas/index.js
@@ -18,11 +18,19 @@ const CACHE_CHECK_EXPIRE = 30 * 60;
 /* and make load time bearable for the user.                                            */
 
 const courseGroupsCache = new NodeCache({ errorOnMissing:true, stdTTL: CACHE_TTL, checkperiod: CACHE_CHECK_EXPIRE });
+const courseGroupCategoriesCache = new NodeCache({ errorOnMissing:true, stdTTL: CACHE_TTL, checkperiod: CACHE_CHECK_EXPIRE });
+const categoryGroupsCache = new NodeCache({ errorOnMissing:true, stdTTL: CACHE_TTL, checkperiod: CACHE_CHECK_EXPIRE });
 const memberCache = new NodeCache({ errorOnMissing:true, stdTTL: CACHE_TTL, checkperiod: CACHE_CHECK_EXPIRE });
 const userCache = new NodeCache({ errorOnMissing:true, stdTTL: CACHE_TTL, checkperiod: CACHE_CHECK_EXPIRE });
 
+courseGroupCategoriesCache.on('expired', function(key) {
+  console.log("[Cache] Expired NodeCache entry for courseGroupCategoriesCache key '" + key + "'.");
+});
 courseGroupsCache.on('expired', function(key) {
   console.log("[Cache] Expired NodeCache entry for courseGroupsCache key '" + key + "'.");
+});
+categoryGroupsCache.on('expired', function(key) {
+  console.log("[Cache] Expired NodeCache entry for categoryGroupsCache key '" + key + "'.");
 });
 memberCache.on('expired', function(key) {
   console.log("[Cache] Expired NodeCache entry for memberCache key '" + key + "'.");
@@ -73,6 +81,106 @@ exports.getCourseGroups = async (courseId) => new Promise(function(resolve, reje
         console.log("[Cache] Data cached for " + CACHE_TTL / 60 + " minutes: " + JSON.stringify(data));
         console.log("[Cache] Statistics: " + JSON.stringify(courseGroupsCache.getStats()));
         console.log("[Cache] Keys: " + courseGroupsCache.keys());
+
+        resolve(data);
+      }
+    });
+  }
+});
+
+// Get group categories for a specified course.
+exports.getGroupCategories = async (courseId) => new Promise(function(resolve, reject) {
+  try {
+    const cachedData = groupCategoriesCache.get(courseId, true);
+    console.log("[Cache] Using found NodeCache entry for courseId " + courseId + ".");
+    console.log("[Cache] Statistics: " + JSON.stringify(groupCategoriesCache.getStats()));
+    resolve(cachedData);
+  }
+  catch {
+    const thisPath = apiPath + "/courses/" + courseId + "/group_categories";
+    console.log("[API] GET " + thisPath);
+
+    request.get({
+      url: thisPath,
+      json: true,
+      headers: {
+        "User-Agent": "Chalmers/Azure/Request",
+        "Authorization": "Bearer " + apiBearerToken
+      }
+    }, 
+    (error, result, data) => {
+      if (error) {
+        console.log("[API] Error: " + error);
+  
+        let err = new Error("Error from API.");
+        err.status = 500;
+  
+        reject(err);
+      }
+      else if (result.statusCode !== 200) {
+        console.log("[API] Status: " + result.statusCode);
+  
+        let err = new Error("Non-OK status code returned from API.");
+        err.status = result.statusCode;
+  
+        reject(err);
+      }
+      else {
+        groupCategoriesCache.set(courseId, data);
+
+        console.log("[Cache] Data cached for " + CACHE_TTL / 60 + " minutes: " + JSON.stringify(data));
+        console.log("[Cache] Statistics: " + JSON.stringify(groupCategoriesCache.getStats()));
+        console.log("[Cache] Keys: " + groupCategoriesCache.keys());
+
+        resolve(data);
+      }
+    });
+  }
+});
+
+// Get groups for a specified category.
+exports.getCategoryGroups = async (categoryId) => new Promise(function(resolve, reject) {
+  try {
+    const cachedData = categoryGroupsCache.get(categoryId, true);
+    console.log("[Cache] Using found NodeCache entry for categoryId " + categoryId + ".");
+    console.log("[Cache] Statistics: " + JSON.stringify(categoryGroupsCache.getStats()));
+    resolve(cachedData);
+  }
+  catch {
+    const thisPath = apiPath + "/group_categories/" + categoryId + "/groups";
+    console.log("[API] GET " + thisPath);
+
+    request.get({
+      url: thisPath,
+      json: true,
+      headers: {
+        "User-Agent": "Chalmers/Azure/Request",
+        "Authorization": "Bearer " + apiBearerToken
+      }
+    }, 
+    (error, result, data) => {
+      if (error) {
+        console.log("[API] Error: " + error);
+  
+        let err = new Error("Error from API.");
+        err.status = 500;
+  
+        reject(err);
+      }
+      else if (result.statusCode !== 200) {
+        console.log("[API] Status: " + result.statusCode);
+  
+        let err = new Error("Non-OK status code returned from API.");
+        err.status = result.statusCode;
+  
+        reject(err);
+      }
+      else {
+        categoryGroupsCache.set(courseId, data);
+
+        console.log("[Cache] Data cached for " + CACHE_TTL / 60 + " minutes: " + JSON.stringify(data));
+        console.log("[Cache] Statistics: " + JSON.stringify(categoryGroupsCache.getStats()));
+        console.log("[Cache] Keys: " + categoryGroupsCache.keys());
 
         resolve(data);
       }
