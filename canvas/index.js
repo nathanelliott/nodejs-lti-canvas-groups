e73269716168ad48284174d478ee7adfbe8f3fb2
Rolf Johansson <rolf.johansson@chalmers.se>
2019-10-17 11:25:37 +0200

    Add NodeCache for caching of API results, test with courseGroups first.

--- canvas/index.js
+++ canvas/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const request = require('request');
+const NodeCache = require('node-cache');
 
 /* This module handles communication between LTI Application and Canvas, using Canvas API V1. */
 /* It requires an API Token, which can be generated from inside Canvas settings.              */
@@ -10,43 +11,59 @@ const request = require('request');
 const apiPath = process.env.canvasApiPath;
 const apiBearerToken = process.env.canvasApiAccessToken;
 
-var userCache = [];
+const CACHE_TTL = 5 * 60;
+const CACHE_CHECK_EXPIRE = 30 * 60;
+
+/* Cache the results of API calls for a shorter period, to ease the load on API servers */
+/* and make load time bearable for the user.                                            */
+
+const courseGroupsCache = new NodeCache({ errorOnMissing:true, stdTTL: CACHE_TTL, checkperiod: CACHE_CHECK_EXPIRE });
+const groupCache = new NodeCache({ errorOnMissing:true, stdTTL: CACHE_TTL, checkperiod: CACHE_CHECK_EXPIRE });
+const memberCache = new NodeCache({ errorOnMissing:true, stdTTL: CACHE_TTL, checkperiod: CACHE_CHECK_EXPIRE });
+const userCache = new NodeCache({ errorOnMissing:true, stdTTL: CACHE_TTL, checkperiod: CACHE_CHECK_EXPIRE });
 
 // Get groups for a specified course.
 exports.getCourseGroups = async (courseId) => new Promise(function(resolve, reject) {
-  console.log("GET " + apiPath + "/courses/" + courseId + "/groups");
+  try {
+    data = courseGroupsCache.get(courseId);
+    console.log("Using found courseGroupsCache entry for courseId " + courseId + ".");
+  } 
+  catch (err) {
+    console.log("No courseGroupsCache entry for courseId " + courseId + ", GET " + apiPath + "/courses/" + courseId + "/groups");
 
-  request.get({
-    url: apiPath + "/courses/" + courseId + "/groups",
-    json: true,
-    headers: {
-      "User-Agent": "Chalmers/Azure/Request",
-      "Authorization": "Bearer " + apiBearerToken
-    }
-  }, 
-  (error, result, data) => {
-    if (error) {
-      console.log("Error: " + error);
-
-      let err = new Error("Error from API.");
-      err.status = 500;
-
-      reject(err);
-    }
-    else if (result.statusCode !== 200) {
-      console.log("Status: " + result.statusCode);
-
-      let err = new Error("Non-OK status code returned from API.");
-      err.status = result.statusCode;
-
-      reject(err);
-    }
-    else {
-      console.log("OK, data: " + JSON.stringify(data));
-
-      resolve(data);
-    }
-  });
+    request.get({
+      url: apiPath + "/courses/" + courseId + "/groups",
+      json: true,
+      headers: {
+        "User-Agent": "Chalmers/Azure/Request",
+        "Authorization": "Bearer " + apiBearerToken
+      }
+    }, 
+    (error, result, data) => {
+      if (error) {
+        console.log("Error: " + error);
+  
+        let err = new Error("Error from API.");
+        err.status = 500;
+  
+        reject(err);
+      }
+      else if (result.statusCode !== 200) {
+        console.log("Status: " + result.statusCode);
+  
+        let err = new Error("Non-OK status code returned from API.");
+        err.status = result.statusCode;
+  
+        reject(err);
+      }
+      else {
+        courseGroupsCache.set(courseId, data);
+        console.log("OK, data: " + JSON.stringify(data));
+        
+        resolve(data);
+      }
+    });
+  }
 });
 
 // Get members for a specified group.
