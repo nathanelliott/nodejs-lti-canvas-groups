8eb6b3fd72a3324a2fb64106b6c1a9849e93af1c
Rolf Johansson <rolf.johansson@chalmers.se>
2019-11-05 15:37:30 +0100

    Trying a different object approach.

--- db/index.js
+++ db/index.js
@@ -50,25 +50,24 @@ exports.getClientData = (userId, env) => new Promise(async function(resolve, rej
 
     db.get("SELECT DISTINCT user_id, user_env, api_token, refresh_token, expires_at_utc FROM tokens WHERE user_id = ? AND user_env = ?", [userId, env],
     function(error, row) {
-        var tokenData;
-        
+        var tokenData = {};
+
         if (error) {
             console.error(error);
             db.close();
             reject(error);
         }
+
         if (row) {
-            tokenData = {
-                access_token: row.api_token,
-                token_type: "Bearer",
-                refresh_token: row.refresh_token,
-                expires_in: 3600,
-                expires_at_utc: new Date(Date.parse(row.expires_at_utc))
-            };
+            tokenData.access_token = row.api_token;
+            tokenData.token_type = "Bearer";
+            tokenData.refresh_token = row.refresh_token;
+            tokenData.expires_in = 3600;
+            tokenData.expires_at_utc = new Date(Date.parse(row.expires_at_utc));
             console.log("Read data from DB: " + JSON.stringify(tokenData));
         }
         else {
-            console.log("No data in db for userId " + user_id);
+            console.log("No data in db for userId " + userId);
             reject(new Error("No data."));
         }
     });
