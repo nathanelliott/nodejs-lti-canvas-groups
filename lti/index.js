3ef0263cc8c718f2459f96fa9b5853bff05edf66
Rolf Johansson <rolf.johansson@chalmers.se>
2020-01-20 13:46:06 +0100

    Log LTI data at other breakpoint.

--- lti/index.js
+++ lti/index.js
@@ -8,8 +8,6 @@ const oauth = require('../oauth');
 const log = require('../log');
 const db = require('../db');
 
-const util = require('util');
-
 const myCache = new NodeCache();
 const nonceStore = new nodeCacheNonceStore(myCache);
 
@@ -71,7 +69,7 @@ exports.handleLaunch = (page) => function(req, res, next) {
         return next(err);
       }
       if (isValid) {
-        log.info("LTI Data:" + JSON.stringify(provider.body)
+        log.info("LTI Data:" + JSON.stringify(provider.body));
 
         if (typeof req.session !== 'undefined' && typeof req.session.token !== 'undefined' && typeof req.session.token.expires_at_utc !== 'undefined') {
           req.session.contextId = provider.context_id;
