7f466657675f47da6f74a63db62947cdf57952f9
Rolf Johansson <rolf.johansson@chalmers.se>
2020-06-08 13:48:34 +0200

    Only run dashboard function if on dashboard page. Add debugLogging as App Setting for LTI communication.

--- lti/index.js
+++ lti/index.js
@@ -15,215 +15,205 @@ const nonceStore = new nodeCacheNonceStore(myCache);
 /* with format "consumer:secret[,consumer2:secret2]".                               */
 
 const consumerKeys = process.env.ltiConsumerKeys;
+const debugLogging = process.env.debugLogging == "true" ? true : false;
 var secrets = [];
 
 const getSecret = (consumerKey, callback) => {
-  if (consumerKeys && secrets.length == 0) {
-    for (const key of consumerKeys.split(',')) {
-      secrets.push({ 
-        "consumerKey": key.split(':')[0], 
-        "secret": key.split(':')[1] 
-      });
+    if (consumerKeys && secrets.length == 0) {
+        for (const key of consumerKeys.split(',')) {
+            secrets.push({
+                "consumerKey": key.split(':')[0],
+                "secret": key.split(':')[1]
+            });
 
-      log.info("Added consumer key for '" + key.split(':')[0] + "'.");
+            log.info("Added consumer key for '" + key.split(':')[0] + "'.");
+        }
     }
-  }
 
-  for (const secret of secrets) {
-    if (secret.consumerKey == consumerKey) {
-      return callback(null, secret.secret);
+    for (const secret of secrets) {
+        if (secret.consumerKey == consumerKey) {
+            return callback(null, secret.secret);
+        }
     }
-  }
 
-  let err = new Error("Unknown consumer '" + consumerKey + "'.");
-  err.status = 403;
+    let err = new Error("Unknown consumer '" + consumerKey + "'.");
+    err.status = 403;
 
-  return callback(err);
+    return callback(err);
 };
 
 exports.handleLaunch = (page) => function(req, res, next) {
-  log.info("[HandleLaunch] Target page: " + page);
-  
-  if (!req.body) {
-    let err = new Error('Expected a body');
-    err.status = 400;
-    return next(err);
-  }
+    log.info("[HandleLaunch] Target page: " + page);
 
-  const consumerKey = req.body.oauth_consumer_key;
-  if (!consumerKey) {
-    let err = new Error('Expected a consumer');
-    err.status = 422;
-    return next(err);
-  }
-
-  getSecret(consumerKey, (err, consumerSecret) => {
-    if (err) {
-      return next(err);
+    if (!req.body) {
+        let err = new Error('Expected a body');
+        err.status = 400;
+        return next(err);
     }
 
-    const provider = new lti.Provider(consumerKey, consumerSecret, nonceStore, lti.HMAC_SHA1);
-    
-    provider.valid_request(req, async (err, isValid) => {
-      if (err) {
+    const consumerKey = req.body.oauth_consumer_key;
+    if (!consumerKey) {
+        let err = new Error('Expected a consumer');
+        err.status = 422;
         return next(err);
-      }
-      if (isValid) {
-        // log.info("LTI Data:" + JSON.stringify(provider.body));
+    }
 
-        if (typeof req.session !== 'undefined' && typeof req.session.token !== 'undefined' && typeof req.session.token.expires_at_utc !== 'undefined') {
-          req.session.contextId = provider.context_id;
-          req.session.contextTitle = provider.context_title;
-          req.session.canvasCourseId = provider.body.custom_canvas_course_id;
-          req.session.canvasEnrollmentState = provider.body.custom_canvas_enrollment_state;
-          req.session.canvasLocale = provider.body.launch_presentation_locale;
-          req.session.canvasApiDomain = provider.body.custom_canvas_api_domain;
-
-          log.info("[Session] Context is " + req.session.contextId + ", course id " + req.session.canvasCourseId + ", " + req.session.contextTitle);
-
-          const now = new Date();
-          const expiry = new Date(Date.parse(req.session.token.expires_at_utc));
-
-          log.info("[Session] User session exists: " + req.session.id + ", expires: " + expiry);
-
-          if (expiry > now) {
-            log.info("[Session] OAuth Token for API is OK.");
-            res.redirect('/' + page);
-          }
-          else if (expiry < now) {
-            log.info("[Session] OAuth Token for API has expired, refreshing.");
-            oauth.providerRefreshToken(req)
-            .then(() => {
-              res.redirect('/' + page);
-            })
-            .catch((error) => {
-              log.error(error);
-
-              if (error.toString().toLowerCase().includes("failed with status code 400")) {
-                log.info("[Session] Token refresh failed with http error 400, redirect to OAuth flow.")
-                res.redirect("/oauth");
-              }
-              else {
-                res.redirect('/error/text/Token+expired+below+but+error+during+refresh+session+exists');
-              }
-            });
-          }
-          else if (expiry == now) {
-            log.info("[Session] The two dates are EXACTLY the same, believe it or not.");
-            oauth.providerRefreshToken(req)
-            .then(() => {
-              res.redirect('/' + page);
-            })
-            .catch((error) => {
-              log.error(error);
-
-              if (error.toString().toLowerCase().includes("failed with status code 400")) {
-                log.info("[Session] Token refresh failed with http error 400, redirect to OAuth flow.")
-                res.redirect("/oauth");
-              }
-              else {
-                res.redirect('/error/text/Token+expired+equal+but+error+during+refresh+session+exists');
-              }
-            });
-          }
-          else {
-            log.info("[Session] No OAuth Token for API, forcing OAuth flow.");
-            res.redirect('/oauth');
-          }
+    getSecret(consumerKey, (err, consumerSecret) => {
+        if (err) {
+            return next(err);
         }
-        else {
-          log.info("[Session] Regenerating session.");
 
-          await req.session.regenerate(err => {
-            if (err) next(err);
-            req.session.contextId = provider.context_id;
-            req.session.contextTitle = provider.context_title;
-            req.session.userId = provider.userId;
-            req.session.username = provider.username;
-            req.session.fullname = provider.body.lis_person_name_full;
-            req.session.email = provider.body.lis_person_contact_email_primary;
-            req.session.ltiConsumer = provider.body.tool_consumer_instance_guid;
-            req.session.isInstructor = provider.instructor === true;
-            req.session.isAdmin = provider.admin === true;
-            req.session.isAlumni = provider.alumni === true;
-            req.session.isContentDeveloper = provider.content_developer === true;
-            req.session.isGuest = provider.guest === true;
-            req.session.isManager = provider.manager === true;
-            req.session.isMentor = provider.mentor === true;
-            req.session.isObserver = provider.observer === true;
-            req.session.isStudent = provider.student === true;
-            req.session.canvasUserId = provider.body.custom_canvas_user_id;
-            req.session.canvasCourseId = provider.body.custom_canvas_course_id;
-            req.session.canvasEnrollmentState = provider.body.custom_canvas_enrollment_state;
-            req.session.canvasLocale = provider.body.launch_presentation_locale;
-            req.session.canvasApiDomain = provider.body.custom_canvas_api_domain;
-          });
+        const provider = new lti.Provider(consumerKey, consumerSecret, nonceStore, lti.HMAC_SHA1);
 
-          log.info("[Session] Regenerated session id: " + req.session.id);
-
-          await db.getClientData(provider.userId, canvas.providerEnvironment(req))
-          .then((value) => {
-            req.session.token = value;
-
-            const now = new Date();
-            const expiry = new Date(Date.parse(req.session.token.expires_at_utc));
-  
-            if (expiry > now) {
-              log.info("[Session] OAuth Token for API is OK.");
-              res.redirect('/' + page);
+        provider.valid_request(req, async(err, isValid) => {
+            if (err) {
+                return next(err);
             }
-            else if (expiry < now) {
-              log.info("[Session] OAuth Token for API has expired, refreshing.");
-              oauth.providerRefreshToken(req)
-              .then(() => {
-                res.redirect('/' + page);
-              })
-              .catch((error) => {
-                log.error(error);
- 
-                if (error.toString().toLowerCase().includes("failed with status code 400")) {
-                  log.info("Token refresh failed with http error 400, redirect to OAuth flow.")
-                  res.redirect("/oauth");
-                }
-                else {
-                  res.redirect('/error/text/Token+expired+below+but+error+during+refresh+session+exists');
-                }
-              });
-            }
-            else if (expiry == now) {
-              log.info("[Session] The two dates are EXACTLY the same, believe it or not.");
-              oauth.providerRefreshToken(req)
-              .then(() => {
-                res.redirect('/' + page);
-              })
-              .catch((error) => {
-                log.error(error);
+            if (isValid) {
+                if (debugLogging)
+                    log.info("LTI Data:" + JSON.stringify(provider.body));
 
-                if (error.toString().toLowerCase().includes("failed with status code 400")) {
-                  log.info("Token refresh failed with http error 400, redirect to OAuth flow.")
-                  res.redirect("/oauth");
+                if (typeof req.session !== 'undefined' && typeof req.session.token !== 'undefined' && typeof req.session.token.expires_at_utc !== 'undefined') {
+                    req.session.contextId = provider.context_id;
+                    req.session.contextTitle = provider.context_title;
+                    req.session.canvasCourseId = provider.body.custom_canvas_course_id;
+                    req.session.canvasEnrollmentState = provider.body.custom_canvas_enrollment_state;
+                    req.session.canvasLocale = provider.body.launch_presentation_locale;
+                    req.session.canvasApiDomain = provider.body.custom_canvas_api_domain;
+
+                    log.info("[Session] Context is " + req.session.contextId + ", course id " + req.session.canvasCourseId + ", " + req.session.contextTitle);
+
+                    const now = new Date();
+                    const expiry = new Date(Date.parse(req.session.token.expires_at_utc));
+
+                    log.info("[Session] User session exists: " + req.session.id + ", expires: " + expiry);
+
+                    if (expiry > now) {
+                        log.info("[Session] OAuth Token for API is OK.");
+                        res.redirect('/' + page);
+                    } else if (expiry < now) {
+                        log.info("[Session] OAuth Token for API has expired, refreshing.");
+                        oauth.providerRefreshToken(req)
+                            .then(() => {
+                                res.redirect('/' + page);
+                            })
+                            .catch((error) => {
+                                log.error(error);
+
+                                if (error.toString().toLowerCase().includes("failed with status code 400")) {
+                                    log.info("[Session] Token refresh failed with http error 400, redirect to OAuth flow.")
+                                    res.redirect("/oauth");
+                                } else {
+                                    res.redirect('/error/text/Token+expired+below+but+error+during+refresh+session+exists');
+                                }
+                            });
+                    } else if (expiry == now) {
+                        log.info("[Session] The two dates are EXACTLY the same, believe it or not.");
+                        oauth.providerRefreshToken(req)
+                            .then(() => {
+                                res.redirect('/' + page);
+                            })
+                            .catch((error) => {
+                                log.error(error);
+
+                                if (error.toString().toLowerCase().includes("failed with status code 400")) {
+                                    log.info("[Session] Token refresh failed with http error 400, redirect to OAuth flow.")
+                                    res.redirect("/oauth");
+                                } else {
+                                    res.redirect('/error/text/Token+expired+equal+but+error+during+refresh+session+exists');
+                                }
+                            });
+                    } else {
+                        log.info("[Session] No OAuth Token for API, forcing OAuth flow.");
+                        res.redirect('/oauth');
+                    }
+                } else {
+                    log.info("[Session] Regenerating session.");
+
+                    await req.session.regenerate(err => {
+                        if (err) next(err);
+                        req.session.contextId = provider.context_id;
+                        req.session.contextTitle = provider.context_title;
+                        req.session.userId = provider.userId;
+                        req.session.username = provider.username;
+                        req.session.fullname = provider.body.lis_person_name_full;
+                        req.session.email = provider.body.lis_person_contact_email_primary;
+                        req.session.ltiConsumer = provider.body.tool_consumer_instance_guid;
+                        req.session.isInstructor = provider.instructor === true;
+                        req.session.isAdmin = provider.admin === true;
+                        req.session.isAlumni = provider.alumni === true;
+                        req.session.isContentDeveloper = provider.content_developer === true;
+                        req.session.isGuest = provider.guest === true;
+                        req.session.isManager = provider.manager === true;
+                        req.session.isMentor = provider.mentor === true;
+                        req.session.isObserver = provider.observer === true;
+                        req.session.isStudent = provider.student === true;
+                        req.session.canvasUserId = provider.body.custom_canvas_user_id;
+                        req.session.canvasCourseId = provider.body.custom_canvas_course_id;
+                        req.session.canvasEnrollmentState = provider.body.custom_canvas_enrollment_state;
+                        req.session.canvasLocale = provider.body.launch_presentation_locale;
+                        req.session.canvasApiDomain = provider.body.custom_canvas_api_domain;
+                    });
+
+                    log.info("[Session] Regenerated session id: " + req.session.id);
+
+                    await db.getClientData(provider.userId, canvas.providerEnvironment(req))
+                        .then((value) => {
+                            req.session.token = value;
+
+                            const now = new Date();
+                            const expiry = new Date(Date.parse(req.session.token.expires_at_utc));
+
+                            if (expiry > now) {
+                                log.info("[Session] OAuth Token for API is OK.");
+                                res.redirect('/' + page);
+                            } else if (expiry < now) {
+                                log.info("[Session] OAuth Token for API has expired, refreshing.");
+                                oauth.providerRefreshToken(req)
+                                    .then(() => {
+                                        res.redirect('/' + page);
+                                    })
+                                    .catch((error) => {
+                                        log.error(error);
+
+                                        if (error.toString().toLowerCase().includes("failed with status code 400")) {
+                                            log.info("Token refresh failed with http error 400, redirect to OAuth flow.")
+                                            res.redirect("/oauth");
+                                        } else {
+                                            res.redirect('/error/text/Token+expired+below+but+error+during+refresh+session+exists');
+                                        }
+                                    });
+                            } else if (expiry == now) {
+                                log.info("[Session] The two dates are EXACTLY the same, believe it or not.");
+                                oauth.providerRefreshToken(req)
+                                    .then(() => {
+                                        res.redirect('/' + page);
+                                    })
+                                    .catch((error) => {
+                                        log.error(error);
+
+                                        if (error.toString().toLowerCase().includes("failed with status code 400")) {
+                                            log.info("Token refresh failed with http error 400, redirect to OAuth flow.")
+                                            res.redirect("/oauth");
+                                        } else {
+                                            res.redirect('/error/text/Token+expired+equal+but+error+during+refresh+session+regenerated');
+                                        }
+                                    });
+                            } else {
+                                log.info("[Session] No OAuth Token for API, forcing OAuth flow.");
+                                res.redirect('/oauth');
+                            }
+                        })
+                        .catch((error) => {
+                            log.error(error);
+                            log.info("[Session] No token data in db for user_id '" + provider.userId + "', forcing OAuth flow.");
+                            res.redirect('/oauth');
+                        });
                 }
-                else {
-                  res.redirect('/error/text/Token+expired+equal+but+error+during+refresh+session+regenerated');
-                }
-              });
+            } else {
+                log.error("[Session] The request is NOT valid.");
+                return next(err);
             }
-            else {
-              log.info("[Session] No OAuth Token for API, forcing OAuth flow.");
-              res.redirect('/oauth');
-            }
-          })
-          .catch((error) => {
-            log.error(error);
-            log.info("[Session] No token data in db for user_id '" + provider.userId + "', forcing OAuth flow.");
-            res.redirect('/oauth');
-          });
-        }
-      }
-      else {
-        log.error("[Session] The request is NOT valid.");
-        return next(err);
-      }
+        });
     });
-  });
 };
--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -3,13 +3,13 @@ function downloadCsv(categoryId, categoryName) {
 }
 
 function renderDashboard() {
-    if($('#chartContainer')) {
+    if ($('#chartContainer')) {
         let data_labels = [];
         var data_reads = [];
         var data_reads_total = 0;
         var data_writes = [];
         var data_writes_total = 0;
-        
+
         $.getJSON('/json/stats').done(function(data) {
             $.each(data.cache_stats, function(index, value) {
                 data_labels.push(value.name);
@@ -18,33 +18,33 @@ function renderDashboard() {
                 data_reads_total = data_reads_total + value.reads;
                 data_writes_total = data_writes_total + value.writes;
             });
-            
+
             $('#active_users').text(data.active_users_today);
             $('#total_users').text(data.authorized_users);
             $('#total_reads').text(data_reads_total);
             $('#total_writes').text(data_writes_total);
-            
-            (data_reads_total > 0 && data_writes_total > 0) ? $('span.percent').text(' (' + Math.round((data_reads_total/data_writes_total)*100) + '%)') : $('span.percent').text(' (0%)');
-            
+
+            (data_reads_total > 0 && data_writes_total > 0) ? $('span.percent').text(' (' + Math.round((data_reads_total / data_writes_total) * 100) + '%)'): $('span.percent').text(' (0%)');
+
             var ctx = document.getElementById('chartContainer').getContext('2d');
-            
+
             var myChart = new Chart(ctx, {
                 type: 'bar',
                 data: {
                     labels: data_labels,
-                    datasets: [
-                    {
-                        label: 'Cache writes',
-                        data: data_writes,
-                        backgroundColor: 'rgba(32, 161, 112, 1)',
-                        borderWidth: 0
-                    },
-                    {
-                        label: 'Cache reads',
-                        data: data_reads,
-                        backgroundColor: 'rgba(144, 144, 88, 1)',
-                        borderWidth: 0
-                    }]
+                    datasets: [{
+                            label: 'Cache writes',
+                            data: data_writes,
+                            backgroundColor: 'rgba(32, 161, 112, 1)',
+                            borderWidth: 0
+                        },
+                        {
+                            label: 'Cache reads',
+                            data: data_reads,
+                            backgroundColor: 'rgba(144, 144, 88, 1)',
+                            borderWidth: 0
+                        }
+                    ]
                 },
                 options: {
                     scales: {
@@ -61,6 +61,8 @@ function renderDashboard() {
 }
 
 $(document).ready(function() {
-    renderDashboard()
-    setInterval(renderDashboard, 600000); // Every 10 mins
+    if (document.location.href.indexOf("/dashboard") > -1) {
+        renderDashboard()
+        setInterval(renderDashboard, 600000); // Every 10 mins
+    }
 });
